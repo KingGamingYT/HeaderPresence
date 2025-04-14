0aad5b54df43127336c277d846c417513a0ff418
KingGamingYT <45918062+KingGamingYT@users.noreply.github.com>
2025-04-13 23:29:55 -0600

    1.2.0 - The Custom Status Update
    adds classic custom status formatting as a toggle

--- HeaderPresence.plugin.js
+++ HeaderPresence.plugin.js
@@ -2,7 +2,7 @@
  * @name HeaderPresence
  * @author KingGamingYT
  * @description See a user's current activities from the header of their user profile, just as it used to be.
- * @version 1.1.0
+ * @version 1.2.0
  */ 
 
 const { Data, Webpack, React, Patcher, DOM, UI } = BdApi;
@@ -19,16 +19,72 @@ const ActivityCard = Webpack.getByStrings("USER_PROFILE_LIVE_ACTIVITY_CARD", "Us
 const SpotifyCard = Webpack.getByStrings("USER_PROFILE_LIVE_ACTIVITY_CARD", "HOVER_ACTIVITY_CARD");
 const VoiceCard = Webpack.getByStrings("USER_PROFILE_VOICE_ACTIVITY_CARD", "OPEN_VOICE_GUILD");
 const StreamCard = Webpack.getByStrings("stream", "OPEN_VOICE_GUILD");
+const EmojiRenderer = Webpack.getByStrings('translateSurrogatesToInlineEmoji');
 const { Button, closeModal } = Webpack.getMangled(/ConfirmModal:\(\)=>.{1,3}.ConfirmModal/, 
     { Button: x=>x.toString?.().includes('submittingFinishedLabel'), 
     closeModal: Webpack.Filters.byStrings(".setState", ".getState()[")});
 const FormSwitch = Webpack.getByStrings('ERROR','tooltipNote', { searchExports: true });
+
+function CustomCards({props, activities}) {
+    const _activities = activities.filter(activity => activity && activity.type === 4);
+    const _emoji = activities.filter(activity => activity.emoji);
+
+    return _activities.map(activity => React.createElement("div", {
+        className: "hp-customStatusContainer",
+        children: [
+            React.createElement("div", { className: "hp-customStatusHeader" }, activity.name),
+            React.createElement("div", { 
+                className: "hp-customStatusContent",
+                children: [
+                    _emoji.map(emoji => React.createElement(EmojiRenderer, { emoji: activity.emoji })),
+                    React.createElement("div", { className: "hp-customStatusText" }, activity.state),
+                ]
+            })
+        ]
+    }))
+}
+function ActivityCards({props, activities}) {
+    const _activities = activities.filter(activity => activity && activity.type !== 4 && activity.name && !activity.name.includes("Spotify"));
+    return _activities.map(activity => React.createElement(ActivityCard, {key: "ac" + activity.created_at, user: props.user, currentUser: UserStore.getCurrentUser(), activity: activity}));
+}
+function SpotifyCards({props, activities}) {
+    const _activities = activities.filter(activity => activity && activity?.type !== 4 && !props.user.bot === true);
+    return _activities.map(activity => React.createElement(SpotifyCard, {key: "sc" + activity.created_at, user: props.user, currentUser: UserStore.getCurrentUser(), activity: activity}));
+}
+function VoiceCards({props, voice}) {
+    const stream = useStateFromStores([ StreamStore ], () => StreamStore.getAnyStreamForUser(props.user.id));
+    const channel = useStateFromStores([ ChannelStore ], () => ChannelStore.getChannel(voice));
+  
+    if (stream || !channel) return;
+    return React.createElement(VoiceCard, {user: props.user, voiceChannel: channel, currentUser: UserStore.getCurrentUser()});
+}
+function StreamCards({props, voice}) {
+    const streams = useStateFromStores([ StreamStore ], () => StreamStore.getAllApplicationStreamsForChannel(voice));
+    const _streams = streams.filter(streams => streams && streams.ownerId == props.user.id);
+    return _streams.map(streams => React.createElement(StreamCard, {user: props.user, stream: streams, currentUser: UserStore.getCurrentUser()}))
+}
 
 const hideActivityPatch = (that, [props]) => {
     if (Data.load('HeaderPresence', 'hideActivity')) {
         if (!props?.items) return;
 
         return props.items = props.items?.filter(item => item.section !== "ACTIVITY");
+    }
+}
+
+const restoreCustomPatch = (that, [props], res) => {
+    const activities = useStateFromStores([ ActivityStore ], () => ActivityStore.getActivities(props.user.id));
+    
+    if (Data.load('HeaderPresence', 'restoreCustom')) {
+        if (!props.profileType?.includes("FULL_SIZE")) return;
+
+        if (Array.isArray(res.props?.children)) {
+            if (activities.length !== 0) {
+                res.props.children.push(
+                    React.createElement(CustomCards, {props, activities})
+                )
+            }
+        };
     }
 }
 
@@ -43,21 +99,35 @@ const settings = {
             else
                 Patcher.unpatchAll('hideActivity');
         }
-	}
+	},
+    restoreCustom: {
+        name: "Restore custom status formatting",
+        note: "Turns custom statuses back into a section of the profile header info.",
+        default: false,
+        changed: (v) => {
+            if (v)
+                Patcher.after("restoreCustom", profileModal, "user", restoreCustomPatch);
+            else
+                Patcher.unpatchAll('restoreCustom');
+        }
+    }
 };
 const changelog = {
     changelog: [
         {
             "title": "Changes",
-            "type" : "improved",
+            "type" : "added",
             "items": [
-                "Plugin release!"
+                "Added a toggle to change how custom statuses are displayed to appear older"
             ]
         }
     ]
 };
 
-const styles = Object.assign({}, Webpack.getByKeys("overlay", "inner"));
+const styles = Object.assign({}, 
+    Webpack.getByKeys("overlay", "inner"), 
+    Webpack.getByKeys("inner", "container"),
+    Webpack.getByKeys("fullSize", "biteSize", "overlay", "outer"));
 
 const statusCSS = webpackify(
     `
@@ -92,9 +162,49 @@ const statusCSS = webpackify(
     .hp-activityContainer:empty { display:none }
 
     .custom-profile-theme {
-        .hp-activityContainer {
+        .hp-activityContainer, .customStatusContainer {
             background: rgb(var(--bg-overlay-color) / var(--bg-overlay-opacity-5));
         }
+    }
+
+    .fullSize:has(.hp-customStatusContainer) .container { 
+        display: none;
+    }
+    .hp-customStatusContainer {
+        padding: 20px 20px 20px 25px;
+        margin-top: 30px;
+        border-radius: var(--radius-sm);
+        background: var(--bg-mod-faint);
+    }
+    .hp-customStatusHeader {
+        font-family: var(--font-display);
+        font-size: 12px;
+        line-height: 1.2857142857142858;
+        font-weight: 600;
+        color: var(--header-secondary);
+        margin-bottom: 8px;
+    }
+    .hp-customStatusContent {
+        .emoji {
+            margin-right: 8px;
+            height: 20px;
+            width: 20px;
+        }
+        .emoji+.hp-customStatusText {
+            display: inline;
+        }
+        &:has(.hp-customStatusText:empty) .emoji {
+            height: 48px;
+            width: 48px;
+        }
+    }
+    .hp-customStatusText {
+        color: var(--header-secondary);
+        font-weight: 500;
+        font-size: 14px;
+    }
+    .fullSize:has(.hp-activityContainer:not(:empty)) .hp-customStatusText { 
+        font-weight: 550;
     }
     `
 )
@@ -136,33 +246,12 @@ module.exports = class HeaderPresence {
                 Data.save('HeaderPresence', key, settings[key].default);
         }
 
-        function ActivityCards({props, activities}) {
-            const _activities = activities.filter(activity => activity && activity.type !== 4 && activity.name && !activity.name.includes("Spotify"));
-            return _activities.map(activity => React.createElement(ActivityCard, {key: "ac" + activity.created_at, user: props.user, currentUser: UserStore.getCurrentUser(), activity: activity}));
-        }
-        function SpotifyCards({props, activities}) {
-            const _activities = activities.filter(activity => activity && activity?.type !== 4 && !props.user.bot === true);
-            return _activities.map(activity => React.createElement(SpotifyCard, {key: "sc" + activity.created_at, user: props.user, currentUser: UserStore.getCurrentUser(), activity: activity}));
-        }
-        function VoiceCards({props, voice}) {
-            const stream = useStateFromStores([ StreamStore ], () => StreamStore.getAnyStreamForUser(props.user.id));
-            const channel = useStateFromStores([ ChannelStore ], () => ChannelStore.getChannel(voice));
-          
-            if (stream || !channel) return;
-            return React.createElement(VoiceCard, {user: props.user, voiceChannel: channel, currentUser: UserStore.getCurrentUser()});
-        }
-        function StreamCards({props, voice}) {
-            const streams = useStateFromStores([ StreamStore ], () => StreamStore.getAllApplicationStreamsForChannel(voice));
-            const _streams = streams.filter(streams => streams && streams.ownerId == props.user.id);
-            return _streams.map(streams => React.createElement(StreamCard, {user: props.user, stream: streams, currentUser: UserStore.getCurrentUser()}))
-        }
-
         DOM.addStyle('statusCSS', statusCSS);
         profileModalTwo.then(profileModalTwo => {Patcher.before("hideActivity", profileModalTwo, "Z", hideActivityPatch)});
+        Patcher.after("restoreCustom", profileModal, "user", restoreCustomPatch);
         Patcher.after("HeaderPresence", profileModal, "user", (that, [props], res) => {
             const activities = useStateFromStores([ ActivityStore ], () => ActivityStore.getActivities(props.user.id));
             const voice = useStateFromStores([ Webpack.getStore('VoiceStateStore') ], () => Webpack.getStore('VoiceStateStore').getVoiceStateForUser(props.user.id)?.channelId);
-
 
             if (!props.profileType?.includes("FULL_SIZE")) return;
             return [
@@ -203,6 +292,7 @@ module.exports = class HeaderPresence {
     stop() {
         Patcher.unpatchAll("HeaderPresence");
         Patcher.unpatchAll("hideActivity");
+        Patcher.unpatchAll('restoreCustom');
         DOM.removeStyle('statusCSS', statusCSS);
     }
 }
